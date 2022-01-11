f82490903f07913118e2ee4bb205f076db0f752c
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-11 15:43:39 +0100

    set header dynamic

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ dotenv.config();
 
 // MIDDLEWARE
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type, Access-Control-Allow-Origin"
