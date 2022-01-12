5e6ae1705f677d68e81885d5e57a551314df13ff
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-12 16:52:43 +0100

    modified d x4

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: `Babatunde_1999`,
-//   host: "localhost",
-//   port: 5432,
-//   database: "store_manager",
-// });
 const pool = new Pool({
-  user: "yunnafvekntzrx",
-  password: `2e8bb0e032e02fc600ea7eb1eb90426d4242ca7fd6d5225467f09b74d8f1d230`,
-  host: "ec2-34-230-198-12.compute-1.amazonaws.com",
+  user: "postgres",
+  password: `Babatunde_1999`,
+  host: "localhost",
   port: 5432,
-  database: "dasck9pf45lf4v",
-  uri: "postgres://yunnafvekntzrx:2e8bb0e032e02fc600ea7eb1eb90426d4242ca7fd6d5225467f09b74d8f1d230@ec2-34-230-198-12.compute-1.amazonaws.com:5432/dasck9pf45lf4v",
+  database: "store_manager",
 });
+// const pool = new Pool({
+//   user: "yunnafvekntzrx",
+//   password: `2e8bb0e032e02fc600ea7eb1eb90426d4242ca7fd6d5225467f09b74d8f1d230`,
+//   host: "ec2-34-230-198-12.compute-1.amazonaws.com",
+//   port: 5432,
+//   database: "dasck9pf45lf4v",
+//   uri: "postgres://yunnafvekntzrx:2e8bb0e032e02fc600ea7eb1eb90426d4242ca7fd6d5225467f09b74d8f1d230@ec2-34-230-198-12.compute-1.amazonaws.com:5432/dasck9pf45lf4v",
+// });
 
 module.exports = pool;
