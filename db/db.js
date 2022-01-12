87969eb13d5b8428a5ec40e7622feebb8404fa42
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-12 11:19:32 +0100

    modified db switched back to zsapi

--- db/db.js
+++ db/db.js
@@ -3,8 +3,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: `Babatunde_1999`,
-  host: "localhost",
-  // host: zsapi.herokuapp.com,
+  // host: "localhost",
+  host: "zsapi.herokuapp.com",
   port: 5432,
   database: "store_manager",
 });
