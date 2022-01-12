8ab97d0f3fe079b1184f4cfaaa210a9390a2dad9
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-12 10:20:33 +0100

    modified db switched back to localhost x2

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: "postgres",
   password: `Babatunde_1999`,
   host: "localhost",
-  host: zsapi.herokuapp.com,
+  // host: zsapi.herokuapp.com,
   port: 5432,
   database: "store_manager",
 });
